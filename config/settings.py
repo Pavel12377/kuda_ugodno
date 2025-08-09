ab751abd6ff54344a9ce126db7c23684f855e2a9
pavel <pstar_88@mail.ru>
2025-08-09 12:56:25 +0300

    [~] правки форматирования R: код был не оптимальным FB: убрал SECRET_KEY из settings

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n3=_tok@(y96ome!$-i^$w_!@9n^lb=9xc6bp)2+)@glg1486&"
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,9 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "hotels.apps.HotelsConfig",
-    "type_of_food.apps.TypeOfFoodConfig",
-    "hotel_rooms.apps.HotelRoomsConfig",
+    "hotels",
+    "type_of_food",
+    "hotel_rooms",
 ]
 
 MIDDLEWARE = [
